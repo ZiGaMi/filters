cc2defd0757132ed7856e82b24c6d0d47a443f39
ziga_miklosic <ziga.miklosic@gmail.com>
2020-12-27 18:53:16 +0100

    - added HPF

--- filter.py
+++ filter.py
@@ -4,12 +4,41 @@ import numpy as np
 
 
 # Time window
-TIME_WINDOW = 20.0 # seconds
+TIME_WINDOW = 10.0 # seconds
 
 # Number of samples in time window
-SAMPLE_NUM = 1000
+SAMPLE_NUM = 10000
+
+# Select input filter signal type
+INPUT_SINE = 0
+INPUT_RECT = 1
+
+# Mux input signal
+INPUT_SIGNAL_SELECTION = INPUT_SINE
 
 
+
+def input_signal_mux(sel, in_1, in_2):
+    if ( INPUT_SINE == sel ):
+        return in_1
+    elif ( INPUT_RECT == sel ):
+        return in_2
+    else:
+        pass
+
+def generate_sine(time, freq, amp, off, phase):
+    return (( amp * np.sin((2*np.pi*freq*time) + phase )) + off )
+
+def generate_rect(time, freq, amp, off, phase):
+    _carier = generate_sine(time, freq, 1.0, 0.0, phase)
+    _sig = 0
+
+    if ( _carier > 0 ):
+        _sig = amp + off
+    else:
+        _sig = off
+
+    return _sig 
 
 class RC_LPF:
 
@@ -26,19 +55,26 @@ class RC_LPF:
         return self.val
 
 
-def generate_sine(time, freq, amp, off, phase):
-    return (( amp * np.sin((2*np.pi*freq*time) + phase )) + off )
+class RC_HPF:
 
-def generate_rect(time, freq, amp, off, phase):
-    _carier = generate_sine(time, freq, amp, off, phase)
-    _sig = 0
+    def __init__(self, fc, dt, order=1, init_val=0):
 
-    if ( _carier > 0 ):
-        _sig = amp + off
-    else:
-        _sin = off
+        # Calculate filter coefficient
+        self.k = (1/fc) / ( dt + 1/fc )
 
-    return _sig
+        # Current value
+        self.val = init_val
+
+        # Prev input value
+        self.x_1 = init_val
+
+    def update(self, x):
+        self.val = ( self.k * self.val + self.k * ( x - self.x_1 ))
+        self.x_1 = x
+
+        return self.val
+
+
         
 
 
@@ -51,37 +87,38 @@ if __name__ == "__main__":
 
     # Filter object
     _filter_1Hz = RC_LPF( fc=1.0, dt=_dt, order=1, init_val=0)
-    _filter_10Hz = RC_LPF( fc=10.0, dt=_dt, order=1, init_val=1)
+    _filter_10Hz = RC_HPF( fc=10.0, dt=_dt, order=1, init_val=0)
 
     # Filter input/output
-    _x = []
-    _sin_x = []
-    _rect_x = []
-
-    # Generate inputs
-    for n in range(SAMPLE_NUM):
-        _sin_x.append( generate_sine( _time[n], .10, 1.0, 0.0, 0.0 ))  
-        _rect_x.append( generate_rect( _time[n], .10, 1.0, 0.0, 0.0 ))  
-
+    _x = [ 0 ] * SAMPLE_NUM
     _y = []
     _y_1 = []
 
+    # Generate inputs
+    _sin_x = []
+    _rect_x = []
+    
+    for n in range(SAMPLE_NUM):
+        _sin_x.append( generate_sine( _time[n], 1.0, 1.0, 0.0, 0.0 ))  
+        _rect_x.append( generate_rect( _time[n], 1.0, 1.0, 0.0, 0.0 ))  
+ 
+
     # Apply filter
     for n in range(SAMPLE_NUM):
-        _y.append( _filter_1Hz.update( _rect_x[n] ) )
-        _y_1.append( _filter_10Hz.update( _rect_x[n] ) )
+        _x[n] = input_signal_mux( INPUT_SIGNAL_SELECTION, _sin_x[n], _rect_x[n] )
+        _y.append( _filter_1Hz.update( _x[n] ) )
+        _y_1.append( _filter_10Hz.update( _x[n] ) )
 
 
     fig, (ax_1, ax_2) = plt.subplots(2, 1)
-   # ax_1.plot( _time, _x, ".-b" )
-    ax_1.plot( _time, _y, ".-r" )
-    ax_1.plot( _time, _y_1, ".-g" )
+    ax_1.plot( _time, _x, "b" )
+    ax_1.plot( _time, _y, "g" )
+    #ax_1.plot( _time, _y_1, ".-" )
     ax_1.grid()
 
-    #ax_2.plot( _time, _y, ".-r" )
-    #ax_2.plot( _time, _y_1, ".-g" )
-    ax_2.plot( _time, _sin_x, ".-r" )
-    ax_2.plot( _time, _rect_x, ".-g" )
+    ax_2.plot( _time, _x, "b" )
+    ax_2.plot( _time, _y_1, "g" )
+    #ax_2.plot( _time, _rect_x, ".-g" )
     ax_2.grid()
 
 
