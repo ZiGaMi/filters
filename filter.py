3300be60fa6bdf36a878a37b6ee6645c5f81c824
ziga_miklosic <ziga.miklosic@gmail.com>
2020-12-31 14:00:38 +0100

    - support high order filters

--- filter.py
+++ filter.py
@@ -14,8 +14,16 @@ INPUT_SINE = 0
 INPUT_RECT = 1
 
 # Mux input signal
-INPUT_SIGNAL_SELECTION = INPUT_SINE
+INPUT_SIGNAL_SELECTION = INPUT_RECT
 
+# Input signal frequency
+INPUT_SIGNAL_FREQ = 10 # Hz
+
+# LPF fc
+LPF_FC = 1.0
+
+# HPF fc
+HPF_FC = 1.0
 
 
 def input_signal_mux(sel, in_1, in_2):
@@ -49,10 +57,22 @@ class RC_LPF:
 
         # Current value
         self.val = init_val
+        self.val_1 = init_val
+
+        # Store order
+        self.order = order
+
+        self.y = [ 0 ] * order
 
     def update(self, x):
-        self.val = ( self.val + self.k * ( x - self.val ))
-        return self.val
+        
+        for n in range( self.order ):
+            if n == 0:
+                self.y[0] = ( self.y[0] + self.k * ( x - self.y[0] ))
+            else:
+                self.y[n] = ( self.y[n] + self.k * ( self.y[n-1] - self.y[n] ))
+
+        return self.y[ self.order - 1 ]
 
 
 class RC_HPF:
@@ -86,33 +106,39 @@ if __name__ == "__main__":
     _time, _dt = np.linspace( 0.0, TIME_WINDOW, num=SAMPLE_NUM, retstep=True )
 
     # Filter object
-    _filter_1Hz = RC_LPF( fc=1.0, dt=_dt, order=1, init_val=0)
-    _filter_10Hz = RC_HPF( fc=10.0, dt=_dt, order=1, init_val=0)
+    _filter_LPF         = RC_LPF( fc=LPF_FC, dt=_dt, order=1, init_val=0)
+    _filter_LPF_2order  = RC_LPF( fc=LPF_FC, dt=_dt, order=2, init_val=0)
+    _filter_HPF         = RC_HPF( fc=HPF_FC, dt=_dt, order=1, init_val=0)
 
     # Filter input/output
     _x = [ 0 ] * SAMPLE_NUM
     _y = []
     _y_1 = []
 
+    _y_lpf_2 = []
+
     # Generate inputs
     _sin_x = []
     _rect_x = []
     
     for n in range(SAMPLE_NUM):
-        _sin_x.append( generate_sine( _time[n], 1.0, 1.0, 0.0, 0.0 ))  
-        _rect_x.append( generate_rect( _time[n], 1.0, 1.0, 0.0, 0.0 ))  
+        _sin_x.append( generate_sine( _time[n], INPUT_SIGNAL_FREQ, 1.0, 0.0, 0.0 ))  
+        _rect_x.append( generate_rect( _time[n], INPUT_SIGNAL_FREQ, 1.0, 0.0, 0.0 ))  
  
 
     # Apply filter
     for n in range(SAMPLE_NUM):
         _x[n] = input_signal_mux( INPUT_SIGNAL_SELECTION, _sin_x[n], _rect_x[n] )
-        _y.append( _filter_1Hz.update( _x[n] ) )
-        _y_1.append( _filter_10Hz.update( _x[n] ) )
+        _y.append( _filter_LPF.update( _x[n] ) )
+        _y_lpf_2.append( _filter_LPF_2order.update( _x[n] ) )
+        
+        _y_1.append( _filter_HPF.update( _x[n] ) )
 
 
     fig, (ax_1, ax_2) = plt.subplots(2, 1)
     ax_1.plot( _time, _x, "b" )
     ax_1.plot( _time, _y, "g" )
+    ax_1.plot( _time, _y_lpf_2, "r" )
     #ax_1.plot( _time, _y_1, ".-" )
     ax_1.grid()
 
