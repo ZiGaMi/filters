e10d95bdfe6154bb526c01e1eddf215439de1e0a
ziga_miklosic <ziga.miklosic@gmail.com>
2021-02-24 21:38:02 +0100

    - added comments, cleaning code

--- simulations/integrator.py
+++ simulations/integrator.py
@@ -24,7 +24,7 @@ import numpy as np
 #   Sample frequency of real system   
 #
 # Unit: Hz
-SAMPLE_FREQ = 10.0
+SAMPLE_FREQ = 1.0
 
 # Ideal sample frequency
 #   As a reference to sample rate constrained embedded system
@@ -35,7 +35,7 @@ IDEAL_SAMPLE_FREQ = 20000.0
 ## Time window
 #
 # Unit: second
-TIME_WINDOW = 1
+TIME_WINDOW = 2.0
 
 
 ## ****** END OF USER CONFIGURATIONS ******
@@ -48,58 +48,94 @@ SAMPLE_NUM = int(( IDEAL_SAMPLE_FREQ * TIME_WINDOW ) + 1.0 )
 #       CLASSES
 # ===============================================================================
 
+# Simple integrator
 class SimpleIntegrator:
 
+    # ===============================================================================
+    # @brief:   Initialize simple integrator
+    #
+    # @param[in]:    fs     - Sample frequency
+    # @return:       void
+    # ===============================================================================
     def __init__(self, fs):
         self.y = 0
         self.dt = 1 / fs
 
+    # ===============================================================================
+    # @brief:   Update integrator
+    #
+    # @param[in]:    x  - Input signal
+    # @return:       y  - Integrated input signal
+    # ===============================================================================
     def update(self, x):
         self.y += x * self.dt
         return self.y
 
+    # ===============================================================================
+    # @brief:   Get current value of integration
+    #
+    # @return: Integrator value
+    # ===============================================================================
     def get(self):
         return self.y
 
+    # ===============================================================================
+    # @brief:   Reset value of integrator
+    #
+    # @return: Integrator value
+    # ===============================================================================
+    def reset(self):
+        self.y = 0
 
+# Trapezoid intergator
 class TrapezoidIntegrator:
-
+    # ===============================================================================
+    # @brief:   Initialize trapezoid integrator
+    #
+    # @param[in]:    fs     - Sample frequency
+    # @return:       void
+    # ===============================================================================
     def __init__(self, fs):
         self.y_prev = 0
         self.x_prev = 0
         self.y = 0
         self.dt = 1 / fs
 
+    # ===============================================================================
+    # @brief:   Update integrator
+    #
+    # @param[in]:    x  - Input signal
+    # @return:       y  - Integrated input signal
+    # ===============================================================================
     def update(self, x):
-        #self.y += ( x + self.x_prev ) * self.dt / 4.0
-        
+
+        # Simple integral
         self.y += x * self.dt
+
+        # Add rectangle
         self.y += ( x - self.x_prev ) * self.dt / 2.0
 
+        # Store for next calculations
         self.x_prev = x
+
         return self.y
 
+    # ===============================================================================
+    # @brief:   Get current value of integration
+    #
+    # @return: Integrator value
+    # ===============================================================================
     def get(self):
         return self.y
 
-
-class KeplerIntegrator:
-    
-    def __init__(self, fs):
-        self.y_prev = 0
-        self.x_prev_1 = 0
-        self.x_prev_2 = 0
+    # ===============================================================================
+    # @brief:   Reset value of integrator
+    #
+    # @return: Integrator value
+    # ===============================================================================
+    def reset(self):
         self.y = 0
-        self.dt = 1 / fs
-
-    def update(self, x):
-        self.y += ( x + 4*self.x_prev_1 + self.x_prev_2 ) * self.dt/6
-        self.x_prev_2 = self.x_prev_1
-        self.x_prev_1 = x
-        return self.y 
-    
-    def get(self):
-        return self.y
+        self.x_prev = 0
 
 
 # ===============================================================================
@@ -115,21 +151,17 @@ if __name__ == "__main__":
 
     # Create integrators
     _simple_int = SimpleIntegrator( fs=SAMPLE_FREQ )
-    _trap_int = TrapezoidIntegrator( fs=SAMPLE_FREQ )
-    _kepler_int = KeplerIntegrator( fs=SAMPLE_FREQ )    
+    _trap_int = TrapezoidIntegrator( fs=SAMPLE_FREQ )  
 
     _ideal_simple_int = SimpleIntegrator( fs=IDEAL_SAMPLE_FREQ )
-    _ideal_trap_int = TrapezoidIntegrator( fs=IDEAL_SAMPLE_FREQ )
-    _ideal_kepler_int = KeplerIntegrator( fs=IDEAL_SAMPLE_FREQ )    
+    _ideal_trap_int = TrapezoidIntegrator( fs=IDEAL_SAMPLE_FREQ )  
 
     # Integrals to plot
     _d_simple_int = [0]
     _d_trap_int = [0]
-    _d_kepler_int = [0]
 
     _i_simple_int = []
     _i_trap_int = []
-    _i_kepler_int = []
 
     # Signal to integrate
     _x_d = [0]
@@ -144,18 +176,16 @@ if __name__ == "__main__":
 
     # Generate ideal signal
     for n, t in enumerate(_time):
-        _input_sig[n] = 10 *t**10
+        _input_sig[n] = t
         #_input_sig[n] = np.sin(t)
         #_input_sig[n] = 20*t**3 - 10*t**2 - 6*t + 10 
         #_input_sig[n] = np.sin( 2*np.pi*1.0*t )**2
 
+        
         """
-        if t > .5:
-            pass#_input_sig[n] = -t + 1
-
         if t > 1.0:
             _input_sig[n] = 1.0
-        """
+        """    
 
     # Apply filter
     for n in range(SAMPLE_NUM):
@@ -173,12 +203,10 @@ if __name__ == "__main__":
             # Perform integration
             _simple_int.update( input )
             _trap_int.update( input )
-            _kepler_int.update( input )
 
             # Store data            
             _d_simple_int.append( _simple_int.get())
             _d_trap_int.append( _trap_int.get())
-            _d_kepler_int.append( _kepler_int.get())
             _d_input.append( input )
 
             # Discrete input
@@ -193,16 +221,13 @@ if __name__ == "__main__":
 
         _ideal_simple_int.update( input )
         _ideal_trap_int.update( input )
-        _ideal_kepler_int.update( input )
 
         _i_simple_int.append( _ideal_simple_int.get())
         _i_trap_int.append( _ideal_trap_int.get())
-        _i_kepler_int.append( _ideal_kepler_int.get())
     
 
     print("Simple integral: %s" % _simple_int.get() )
     print("Trap integral: %s" % _trap_int.get() )
-    print("Kepler integral: %s" % _kepler_int.get() )
 
 
     # =============================================================================================
@@ -229,16 +254,10 @@ if __name__ == "__main__":
     ax[0].plot(_time, _input_sig, label="ideal")
     ax[0].plot(_d_time, _d_input, ".y", label="sampled")
 
-    # real
-   
     ax[1].plot(_d_time, _d_simple_int, ".-w", label="simp")
     ax[1].plot(_d_time, _d_trap_int, ".-y", label="trap")
-    ax[1].plot(_d_time, _d_kepler_int, ".-r", label="kep")
 
-    # ideal
-    #ax[1].plot(_time, _i_simple_int, "w", label="simple")
-    #ax[1].plot(_time, _i_trap_int, "w", label="trap")
-    #ax[1].plot(_time, _i_kepler_int, "w", label="simp")
+
     
 
 
